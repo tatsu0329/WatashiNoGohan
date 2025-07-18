916e365db3781fdd9b9b3902e067c8a4816345d4
Tatsuki ksto <tatsu.0329.gran.turismo@gmail.com>
2025-07-18 20:35:23 +0900

    Initial Commit

---
+++ WatashiNoGohan/WatashiNoGohanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatashiNoGohanApp.swift
+//  WatashiNoGohan
+//
+//  Created by Tatsuki Kato on 2025/07/18.
+//
+
+import SwiftUI
+
+@main
+struct WatashiNoGohanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
